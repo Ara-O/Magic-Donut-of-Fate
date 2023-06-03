7d1d0a10344ecb924b4fe77ca719a4d72d521271
Ara <oladipoeyiara@gmail.com>
2023-06-03 14:32:05 -0400

    Adding transition

--- magic-donut/src/App.vue
+++ magic-donut/src/App.vue
@@ -14,10 +14,7 @@
         src="./assets/donut.png"
         class="w-10 cursor-pointer mt-4"
         alt="Donut image"
-        @click="
-          userIsWorthy = true;
-          worthinessHasBeenDecided = true;
-        "
+        @click="enterDonutPresence"
       />
     </span>
     <span class="flex flex-col items-center" v-else>
@@ -38,48 +35,50 @@
       />
     </span>
   </main>
-  <main
-    v-show="worthinessHasBeenDecided"
-    class="flex justify-center items-center h-screen flex-col"
-  >
-    <canvas
-      class="bg-gray-400 bg-transparent"
-      height="300"
-      width="300"
-      ref="donutCanvas"
-    ></canvas>
-    <section class="text-center w-96">
-      <h3 class="text-lg tracking-wide font-medium">
-        Ask the Magic Donut a question
-      </h3>
+  <Transition name="fade-in">
+    <main
+      v-show="worthinessHasBeenDecided"
+      class="flex justify-center items-center h-screen flex-col"
+    >
+      <canvas
+        class="bg-gray-400 bg-transparent"
+        height="300"
+        width="300"
+        ref="donutCanvas"
+      ></canvas>
+      <section class="text-center w-96">
+        <h3 class="text-lg tracking-wide font-medium">
+          Ask the Magic Donut a question
+        </h3>
 
-      <span class="flex justify-center mt-6 items-center gap-5">
-        <input
-          type="text"
-          v-model="userQuestion"
-          class="h-11 w-72 px-4 font-light rounded-[4px] border-[0.01px] border-[#ffffffba] bg-transparent text-center text-sm outline-none"
-          placeholder="Ask and ye shall receive"
-        />
-        <button
-          class="transition ease-in-out hover:animate-spin"
-          @click="receiveFate"
-        >
-          <img
-            src="./assets/donut.png"
-            class="w-9 cursor-pointer"
-            alt="Donut image"
-            ref="donutButtonImage"
+        <span class="flex justify-center mt-6 items-center gap-5">
+          <input
+            type="text"
+            v-model="userQuestion"
+            class="h-11 w-72 px-4 font-light rounded-[4px] border-[0.01px] border-[#ffffffba] bg-transparent text-center text-sm outline-none"
+            placeholder="Ask and ye shall receive"
           />
-        </button>
-      </span>
+          <button
+            class="transition ease-in-out hover:animate-spin"
+            @click="receiveFate"
+          >
+            <img
+              src="./assets/donut.png"
+              class="w-9 cursor-pointer"
+              alt="Donut image"
+              ref="donutButtonImage"
+            />
+          </button>
+        </span>
 
-      <div class="h-56">
-        <h5 class="leading-7 mt-5 font-light text-[14.5px]">
-          {{ userFate }}
-        </h5>
-      </div>
-    </section>
-  </main>
+        <div class="h-56">
+          <h5 class="leading-7 mt-5 font-light text-[14.5px]">
+            {{ userFate }}
+          </h5>
+        </div>
+      </section>
+    </main>
+  </Transition>
 </template>
 
 <script setup lang="ts">
@@ -119,6 +118,11 @@ async function receiveFate() {
   console.log("ahh");
 }
 
+function enterDonutPresence() {
+  userIsWorthy.value = true;
+  worthinessHasBeenDecided.value = true;
+}
+
 function moveDonutButtonPosition() {
   let xValue = Math.random() * window.innerWidth;
   let yValue = Math.random() * window.innerHeight;
@@ -151,6 +155,15 @@ onMounted(async () => {
   transition: all 250ms;
 }
 
+.fade-in-enter-active,
+.fade-in-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-in-enter-from,
+.fade-in-leave-to {
+  opacity: 0;
+}
 @keyframes DonutBounce {
   0% {
     transform: scale(1);
